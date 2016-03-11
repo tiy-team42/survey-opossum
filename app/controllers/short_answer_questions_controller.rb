7093c1c26f4453caf246935f1a3cfb0cebbaf4cd
Publicover <jameslpublicover@gmail.com>
2016-03-11 09:18:29 -0500

    authentication doesn't quite work--pushing so we can hit HTML for login page.

--- app/controllers/short_answer_questions_controller.rb
+++ app/controllers/short_answer_questions_controller.rb
@@ -2,9 +2,6 @@ class ShortAnswerQuestionsController < ApplicationController
   before_action :logged_in?
   before_action :set_short_answer_question, only: [:show, :edit, :update, :destroy]
 
-  def logged_in?
-    redirect_to login_path, notice: "You must log in to access this page." unless session[:user_id]
-  end
   # GET /short_answer_questions
   def index
     @short_answer_questions = ShortAnswerQuestion.all
