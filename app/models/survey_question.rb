4794bb7dcb331bad2fb0528e76045236321f5211
Kate Studwell <katestudwell@gmail.com>
2016-03-11 09:02:47 -0500

    Add methods to survey questions to determine question type

--- app/models/survey_question.rb
+++ app/models/survey_question.rb
@@ -8,4 +8,16 @@ class SurveyQuestion < ActiveRecord::Base
   accepts_nested_attributes_for :short_answer_questions
   accepts_nested_attributes_for :long_answer_questions
 
+  def is_boolean?
+    question_type == "BooleanQuestion"
+  end
+
+  def is_short_answer?
+    question_type == "ShortAnswerQuestion"
+  end
+
+  def is_long_answer?
+    question_type == "LongAnswerQuestion"
+  end
+
 end
