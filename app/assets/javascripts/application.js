5262b2cf9406efa2ee15f9913476dc57222c07c2
Byron Wall <byrondevonwall@gmail.com>
2016-03-13 12:31:31 -0400

    add position counting to question append

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -26,6 +26,7 @@ $('.add-question').on("click", function(){
   // console.log(clickCount);
   $('.question-wrap').append("<div class='pick-question' id='question"+clickCount+"'><span class='move-up'>^</span><span class='move-down'>D</span><span class='q-txt'>QUESTION: <input placeholder='ASK YOUR QUESTION HERE' class='q-field' type='text' name='survey[survey_questions_attributes]["+clickCount+"][text]' id='survey_survey_questions_attributes_"+clickCount+"_text'></span><span class='q-options'><span class='q-required'>Required?: <input name='survey[survey_questions_attributes]["+clickCount+"][required]'' type='hidden' value='"+clickCount+"'><input type='checkbox' value='1' name='survey[survey_questions_attributes]["+clickCount+"][required]' id='survey_survey_questions_attributes_"+clickCount+"_required'></span><span class='q-type'>This Question is a: <select name='survey[survey_questions_attributes]["+clickCount+"][question_type]' id='survey_survey_questions_attributes_"+clickCount+"_question_type'><option value=''>Select question type...</option><option value='BooleanQuestion'>Yes No</option><option value='ShortAnswerQuestion'>Short Answer</option><option value='LongAnswerQuestion'>Long Answer</option><option value='DropdownQuestion'>Multiple Choice Dropdown</option></select></span>Remove?<input class='q-remove' type='checkbox' value='1' name='survey[survey_questions_attributes]["+clickCount+"][_destroy]' id='survey_survey_questions_attributes"+clickCount+"__destroy'></span><span class='hidden'> The first answer should be the 'Yes' option. For multiple choice, add as many as you'd like!<input value='Yes, No' type='text' name='survey[survey_questions_attributes]["+clickCount+"][answer_options]' id='survey_survey_questions_attributes_"+clickCount+"_answer_options'></span><input name='survey[survey_questions_attributes]["+clickCount+"][_destroy]'' type='hidden' value='"+clickCount+"'><input type='text' class='q-position hidden' name='survey[survey_questions_attributes]["+clickCount+"][position] id='survey_survey_questions_attributes_"+clickCount+"_position'></div>");
   doWhatMyHeartTellsMe()
+  questionPosition()
 });
 
 //this function removes questions
@@ -38,22 +39,26 @@ $('.question-wrap').on("click", '.q-remove', function(){
 $('.question-wrap').on('click', '.move-up', function(){
 
   $(this).parents('.pick-question').insertBefore($(this).parents('.pick-question').prev());
-    $('.q-position').each(function(index){
-    $(this).val(index);
+  questionPosition()
     });
 });
 
 //this function moves questions down
 $('.question-wrap').on('click', '.move-down', function(){
   $(this).parents('.pick-question').insertAfter($(this).parents('.pick-question').next());
-  $('.q-position').each(function(index){
-  $(this).val(index);
+  questionPosition()
   });
 });
 
+//this pushes the scroll
 function doWhatMyHeartTellsMe(){
   var position = $(".question-wrap").prop('scrollHeight');
   $(".question-wrap").scrollTop(position);
 }
 
+//this tells rails the position
+function questionPosition(){
+  $('.q-position').each(function(index){
+  $(this).val(index);
+}
 });//close iify
