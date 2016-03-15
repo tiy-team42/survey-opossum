aa0b2437cee1a2b45d709aa57d3e4e1dbf85a780
Kate Studwell <katestudwell@gmail.com>
2016-03-14 20:21:18 -0400

    Add logic to results page to not show empty responses

--- app/models/dropdown_question.rb
+++ app/models/dropdown_question.rb
@@ -1,10 +1,19 @@
 class DropdownQuestion < ActiveRecord::Base
   belongs_to :survey_question
   validate :is_required, on: :create
+  # before_create :check_answer_exists
 
   def is_required
     if answer.empty? && survey_question.required
       errors.add(:answer, "This is a required field")
+    end
+  end
+
+  def check_answer_exists
+    if answer.empty?
+      false
+    else
+      true
     end
   end
 
